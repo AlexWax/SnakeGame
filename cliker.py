5ec1605cb4e3f3e6e05f67f36e4df974e4467d2e
AlexWax <leha.voskoboynik@mail.ru>
2024-12-20 00:33:46 +0300

    Technicaly, but not real

--- cliker.py
+++ cliker.py
@@ -1,4 +1,5 @@
 import datetime
+import random
 from tkinter import *
 from time import *
 
@@ -6,10 +7,8 @@ from time import *
 class Click:
     def __init__(self):
         self.width = 1536-970
-        self.R_max = 96
-        self.speed_change = 3
-        self.speed_change_2 = 3
-        self.delay_new_circle = 10
+        self.speed_change = 2
+        self.factor = 0.5
 
         self.main_window()
         self.binds()
@@ -31,40 +30,23 @@ class Click:
         y = arg.y
 
         self.counter_1 = 0
-        self.counter_2 = 0
-        self.R_1 = 0
-        self.R_2 = 0
-        self.crl_outline_width_1 = 10
-        self.crl_outline_width_2 = 10
+        self.R_1 = 10
         self.main_canvas.delete('circle')
 
         def rec():
-            print({1: datetime.datetime.now()})
-            if self.R_1 < 96:
+            if self.counter_1 < 50:
                 self.R_1 += 1
                 self.counter_1 += 1
-                self.crl_outline_width_1 -= 0.1
                 self.main_canvas.delete('circle_rad')
-                circle_2 = self.main_canvas.create_oval(x - self.R_1, y - self.R_1, x + self.R_1, y + self.R_1,
-                                                        fill='', width=self.crl_outline_width_1,
-                                                        outline='blue', tags='circle_rad', )
-                self.window.after(self.speed_change, rec_2)
+                [self.main_canvas.create_oval(x - (self.R_1 - 10*i)*self.factor, y - (self.R_1 - 10*i)*self.factor, x + (self.R_1 - 10*i)*self.factor, y + (self.R_1 - 10*i)*self.factor,
+                                            fill='', width=random.randint(1, round((i+1)/2)),
+                                            outline='blue', tags='circle_rad') for i in range(1, 5) if self.counter_1 > i*5]
 
-        def rec_2():
-            print({2: datetime.datetime.now()})
-            if self.R_2 < 96:
-                self.R_2 += 1
-                self.counter_2 += 1
-                self.crl_outline_width_2 -= 0.1
-                self.main_canvas.delete('circle_rad_2')
-                circle_2 = self.main_canvas.create_oval(x - self.R_2, y - self.R_2, x + self.R_2, y + self.R_2,
-                                                        fill='', width=self.crl_outline_width_2,
-                                                        outline='green', tags='circle_rad_2', )
-                self.window.after(self.speed_change_2, rec)
+                self.window.after(self.speed_change, rec)
 
 
         rec()
-        self.window.after(100)
+
 
 
     def binds(self):
