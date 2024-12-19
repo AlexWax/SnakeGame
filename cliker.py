2184137ba38282ca3ebf41f6840ea38130896a8c
AlexWax <leha.voskoboynik@mail.ru>
2024-12-19 23:29:19 +0300

    Problem with delay bitween two circles

--- cliker.py
+++ cliker.py
@@ -1,11 +1,15 @@
+import datetime
 from tkinter import *
+from time import *
 
 
 class Click:
     def __init__(self):
         self.width = 1536-970
-        self.R_1 = 50
+        self.R_max = 96
         self.speed_change = 3
+        self.speed_change_2 = 3
+        self.delay_new_circle = 10
 
         self.main_window()
         self.binds()
@@ -25,29 +29,43 @@ class Click:
     def water_click(self, arg):
         x = arg.x
         y = arg.y
-        self.main_canvas.delete('circle')
-        circle = self.main_canvas.create_oval(x-self.R_1, y-self.R_1, x+self.R_1, y+self.R_1,
-                                              fill='grey', outline='', tags='circle',)
 
-        self.counter = 0
+        self.counter_1 = 0
+        self.counter_2 = 0
         self.R_1 = 0
+        self.R_2 = 0
+        self.crl_outline_width_1 = 10
+        self.crl_outline_width_2 = 10
+        self.main_canvas.delete('circle')
 
         def rec():
-            if self.counter < 96:
-                self.counter += 1
-                self.main_canvas.itemconfig(circle, fill='grey'+str(self.counter))
-                self.window.after(self.speed_change, rec)
-
-        def rec_2():
-            if self.R_1 < 50:
+            print({1: datetime.datetime.now()})
+            if self.R_1 < 96:
                 self.R_1 += 1
-                self.main_canvas.delete('circle')
+                self.counter_1 += 1
+                self.crl_outline_width_1 -= 0.1
+                self.main_canvas.delete('circle_rad')
                 circle_2 = self.main_canvas.create_oval(x - self.R_1, y - self.R_1, x + self.R_1, y + self.R_1,
-                                                        fill='', outline='black', tags='circle', )
+                                                        fill='', width=self.crl_outline_width_1,
+                                                        outline='blue', tags='circle_rad', )
                 self.window.after(self.speed_change, rec_2)
 
+        def rec_2():
+            print({2: datetime.datetime.now()})
+            if self.R_2 < 96:
+                self.R_2 += 1
+                self.counter_2 += 1
+                self.crl_outline_width_2 -= 0.1
+                self.main_canvas.delete('circle_rad_2')
+                circle_2 = self.main_canvas.create_oval(x - self.R_2, y - self.R_2, x + self.R_2, y + self.R_2,
+                                                        fill='', width=self.crl_outline_width_2,
+                                                        outline='green', tags='circle_rad_2', )
+                self.window.after(self.speed_change_2, rec)
+
+
         rec()
-        rec_2()
+        self.window.after(100)
+
 
     def binds(self):
         self.main_canvas.bind('<Button-1>', self.water_click)
