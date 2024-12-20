37b1ffa5255721c21c435666ba8116b9b4811d57
AlexWax <leha.voskoboynik@mail.ru>
2024-12-20 21:10:51 +0300

    Not ideal, butstill better

--- cliker.py
+++ cliker.py
@@ -7,8 +7,7 @@ from time import *
 class Click:
     def __init__(self):
         self.width = 1536-970
-        self.speed_change = 2
-        self.factor = 0.5
+
 
         self.main_window()
         self.binds()
@@ -28,26 +27,28 @@ class Click:
     def water_click(self, arg):
         x = arg.x
         y = arg.y
-
+        self.speed_change = random.randint(2, 3)
+        self.nums_of_ovals = random.randint(4, 6)
+        print(self.nums_of_ovals)
         self.counter_1 = 0
         self.R_1 = 10
-        self.main_canvas.delete('circle')
+        self.dist_b_ovals = [random.randint(3, 10) / 10 for i in range(self.nums_of_ovals - 1)]
 
         def rec():
-            if self.counter_1 < 50:
+            if self.counter_1 < 125:
                 self.R_1 += 1
                 self.counter_1 += 1
+
                 self.main_canvas.delete('circle_rad')
-                [self.main_canvas.create_oval(x - (self.R_1 - 10*i)*self.factor, y - (self.R_1 - 10*i)*self.factor, x + (self.R_1 - 10*i)*self.factor, y + (self.R_1 - 10*i)*self.factor,
+                ovals = [self.main_canvas.create_oval(x-2 - (self.R_1 - 10*i)*self.dist_b_ovals[i-1], y+2 - (self.R_1 - 10*i)*self.dist_b_ovals[i-1],
+                                              x + (self.R_1 - 10*i)*self.dist_b_ovals[i-1], y + (self.R_1 - 10*i)*self.dist_b_ovals[i-1],
                                             fill='', width=random.randint(1, round((i+1)/2)),
-                                            outline='blue', tags='circle_rad') for i in range(1, 5) if self.counter_1 > i*5]
+                                            outline='dodgerblue', tags='circle_rad') for i in range(1, self.nums_of_ovals) if self.counter_1 > i*self.dist_b_ovals[i-1]]
+                [self.main_canvas.itemconfig(ovals[i], width=0) for i in range(self.nums_of_ovals-1) if self.R_1 >= 90 + 10*i]
 
                 self.window.after(self.speed_change, rec)
 
-
         rec()
-
-
 
     def binds(self):
         self.main_canvas.bind('<Button-1>', self.water_click)
